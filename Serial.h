201ea9ae578f883c99ff5cff1cddd7856615ad0b
Mandar D <38375621+mandardharaskar@users.noreply.github.com>
2018-08-30 11:16:30 +0530

    Create Serial.h
    ccs header file

---
+++ Serial.h
@@ -0,0 +1,39 @@
+void Serial_begin()
+{
+  P4SEL |= BIT4+BIT5;                       // P3.3,4 = USCI_A0 TXD/RXD
+  UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+  UCA1CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA1BR0 = 9;                              // 1MHz 115200 (see User's Guide)
+  UCA1BR1 = 0;                              // 1MHz 115200
+  UCA1MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
+  UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
+  __no_operation();                         // For debugger
+
+}
+// Echo back RXed character, confirm TX buffer is ready first
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch(__even_in_range(UCA1IV,4))
+  {
+  case 0:break;                             // Vector 0 - no interrupt
+  case 2:                                   // Vector 2 - RXIFG
+    while (!(UCA1IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+    Serial_print();
+    break;
+  case 4:break;                             // Vector 4 - TXIFG
+  default: break;
+  }
+}
+
+
+
+
